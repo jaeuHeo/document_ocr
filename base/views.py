cc81902a5161e7a0d96d834e3358cc642220e5fd
jaeuHeo <hju0594@gmail.com>
2022-02-06 19:00:40 +0900

    None

--- base/views.py
+++
@@ -1,9 +0,0 @@
-from django.conf import settings
-from django.views.static import serve
-
-def show_static(request):
-    print('doc_classifier/static/')
-    path = request.GET.get('img_name', None)
-    # path = request.data.copy()['img_name']
-    print('path',path)
-    return serve(**{'request':request, 'document_root':settings.MEDIA_ROOT, 'path':path})
